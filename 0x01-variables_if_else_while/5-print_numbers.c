85059ee7dd4b1e418cf07b9105206717df6a56e8
Andrewingit <alexanderobohandrew@gmail.comgit config --global user.email alexanderobohandrew@gmail.com>
2022-09-09 15:28:40 -0700

    commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,17 +2,17 @@
 /**
 *main - print if the number is positive, zero, or negative
 *
-*Description: using the main function 
+*Description: using the main function
 *this program prints "Programming is positive, zero, or negative
 *Return: 0
 */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+int i;
+for (i = 0; i < 10; i++)
 {
-putchar(ch);
+printf("%i", i);
 }
-putchar('\n');
+printf("\n");
 return (0);
 }
