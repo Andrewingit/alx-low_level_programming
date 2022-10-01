96e5f049e7c8f5744bcb4d8fa7a4c853389a7053
Andrewingit <alexanderobohandrew@gmail.comgit config --global user.email alexanderobohandrew@gmail.com>
2022-09-30 17:20:43 -0700

    message

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
-*File: main.h
-*Auth:Andrew Oboh
-*Desc: Header file containing the prototypes of al functions
-*/
 
 int _putchar(char c);
 int _islower(int c);
@@ -28,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
